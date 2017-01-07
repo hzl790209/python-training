f422887053eb1aa38d9290abb0e7880d361eebe8
hzl790209 <huzhongliang790209@hotmail.com>
2017-01-07 16:06:59 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,11 @@
+age =raw_input('hello old are you?:')
+job =raw_input('what is your job')
+
+if int(age)>=50:
+    print "you r to old"
+
+elif int(age)<=29:
+    print "good age"
+
+else:
+    print "I dont know wfk "
